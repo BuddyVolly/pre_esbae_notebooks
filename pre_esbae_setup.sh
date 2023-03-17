a48c710510fec3e70a730cc143c26a9b19d154eb
BuddyVolly <andreasvollrath@gmx.de>
2023-03-17 16:22:56 +0000

    resolving bash setup

--- pre_esbae_setup.sh
+++ pre_esbae_setup.sh
@@ -7,10 +7,13 @@ do
     esac
 done
 
-folder_name="~/venv/pre_esbae"
-mkdir -p $folder_name # Create your folder (included partents if are given).
-python3 -m venv $folder_name # Create the venv, this line could take some time.
-source $folder_name/bin/activate # Activate the virtual enviroment just created.
+
+mkdir -p "venv"
+cd venv
+mkdir -p "pre_esbae"
+# Create your folder (included partents if are given).
+python3 -m venv "pre_esbae" # Create the venv, this line could take some time.
+source "pre_sbae/bin/activate" # Activate the virtual enviroment just created.
 pip install ipykernel # Install ipykernel in our venv.
 python -m ipykernel install --user --name="pre_esbae" --display-name="pre_esbae" 
 
