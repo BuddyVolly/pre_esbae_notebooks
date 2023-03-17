f3878a867424edb0260173ce88d93a7cbfd4d776
BuddyVolly <andreasvollrath@gmx.de>
2023-03-17 16:12:57 +0000

    updating setup

--- pre_esbae_setup.sh
+++ pre_esbae_setup.sh
@@ -8,7 +8,7 @@ do
 done
 
 folder_name="~/venv/pre_esbae"
-mkdir -p "folder_name" # Create your folder (included partents if are given).
+mkdir -p "$folder_name" # Create your folder (included partents if are given).
 python3 -m venv "$folder_name" # Create the venv, this line could take some time.
 source "$folder_name/bin/activate" # Activate the virtual enviroment just created.
 pip install ipykernel # Install ipykernel in our venv.
