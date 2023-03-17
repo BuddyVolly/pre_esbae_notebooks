340dadc04aea5f91a485e0992433b7d97ad250a0
BuddyVolly <andreasvollrath@gmx.de>
2023-03-17 16:16:11 +0000

    updating setup

--- pre_esbae_setup.sh
+++ pre_esbae_setup.sh
@@ -8,11 +8,11 @@ do
 done
 
 folder_name="~/venv/pre_esbae"
-mkdir -p "$folder_name" # Create your folder (included partents if are given).
-python3 -m venv "$folder_name" # Create the venv, this line could take some time.
-source "$folder_name/bin/activate" # Activate the virtual enviroment just created.
+mkdir -p $folder_name # Create your folder (included partents if are given).
+python3 -m venv $folder_name # Create the venv, this line could take some time.
+source $folder_name/bin/activate # Activate the virtual enviroment just created.
 pip install ipykernel # Install ipykernel in our venv.
-python -m ipykernel install --user --name="$folder_name" --display-name="(venv) - pre_esbae" 
+python -m ipykernel install --user --name="pre_esbae" --display-name="pre_esbae" 
 
 pip install "git+https://github.com/openforis/earthengine-api.git@v0.1.343#egg=earthengine-api&subdirectory=python"
 pip install "git+https://github.com/BuddyVolly/eSBAE"
